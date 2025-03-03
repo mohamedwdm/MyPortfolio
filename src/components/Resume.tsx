9d35dac4a34d0e026662027965395904c5a1fc09
unknown <wdm46857@gmail.com>
2025-03-03 16:15:30 +0200

    new

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -42,7 +42,7 @@ export default function Resume() {
 
             <div className="flex flex-col sm:flex-row justify-center gap-4">
               <motion.a
-                href={"https://drive.google.com/file/d/1VF8BMwOeade0tEkfXHjIGBIBjcUkX2xg/view?usp=sharing"}
+                href={"https://drive.google.com/file/d/1sOPHVPYTCdtxe3bMhWYAG1r9viE03IPk/view?usp=sharing"}
                 target="_blank"
                 download
                 className={`flex items-center justify-center gap-2 px-8 py-3 rounded-full text-white bg-gradient-to-r from-purple-600 via-pink-600 to-blue-600 hover:from-purple-700 hover:via-pink-700 hover:to-blue-700 transition-all duration-300 shadow-lg shadow-purple-500/25 dark:shadow-purple-500/10`}
